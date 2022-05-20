e0f02c15fb6fa5f31bed53713ffcd7e4b63eeddc
Iván Rodríguez <80779130+drodriguezcimat@users.noreply.github.com>
2022-05-20 14:39:41 -0500

    Cambio en el título (versión 1.0)

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
     # Application title
   
     #GIT - EDITAR TITULO DE LA APLICACION ###################################
-    titlePanel("Iris Database: Classification with KNN"),
+    titlePanel("Iris Database: Classification with KNN (versión 1.0)"),
     #########################################################################
 
     # Sidebar with a slider input for number of bins 
